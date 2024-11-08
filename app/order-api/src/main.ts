99992830a3c21736703131983fddb34e111f237a
tuanhcmute <20110743@student.hcmute.edu.vn>
2024-11-08 10:49:07 +0700

    init deploy using teraform & init workflows

--- app/order-api/src/main.ts
+++ app/order-api/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get('PORT') || 8080;
   const version = configService.get('VERSION');
 
   app.setGlobalPrefix(`api/${version}`);
