f227e0f66d62f16623577320a77102635de112d2
doduongthaituan <doduongthaituan@201102@gmail.com>
2024-11-21 09:33:05 +0700

    modify get menu, add get all error codes

--- app/order-api/src/app/app.controller.ts
+++ app/order-api/src/app/app.controller.ts
@@ -1,16 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from 'src/auth/public.decorator';
-import { ApiExcludeController } from '@nestjs/swagger';
+import { ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
+import { TErrorCode } from './app.validation';
+import { AppResponseDto } from './app.dto';
 
+@ApiTags('App')
 @Controller()
-@ApiExcludeController(true)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
   @Public()
+  @ApiExcludeEndpoint()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('error-codes')
+  @Public()
+  getErrorCodes(): AppResponseDto<TErrorCode> {
+    const result = this.appService.getErrorCodes();
+    return {
+      message: 'Error codes have been retrieved successfully',
+      statusCode: HttpStatus.OK,
+      timestamp: new Date().toISOString(),
+      result,
+    } as AppResponseDto<TErrorCode>;
   }
 }
--- app/order-api/src/app/app.service.ts
+++ app/order-api/src/app/app.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AppValidation, TErrorCode } from './app.validation';
 
 @Injectable()
 export class AppService {
+  getErrorCodes(): TErrorCode {
+    return AppValidation;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
