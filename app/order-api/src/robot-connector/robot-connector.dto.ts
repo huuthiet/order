6ae903aefbb75ce06cba59a6584fb9bba92c25ac
huu thiet <huuthietn01@gmail.com>
2024-11-26 10:30:03 +0700

    add: add robot connector client for qr locatioin

--- app/order-api/src/robot-connector/robot-connector.dto.ts
+++ app/order-api/src/robot-connector/robot-connector.dto.ts
@@ -1,12 +1,34 @@
-export class InitiateWorkFlowInstanceRequestDto {
-  orderCode: string;
+export class CreateWorkFlowRequestDto {
+  name: string;
+  description: string;
+}
+export class WorkFlowResponseDto {
+  id: string;
+  name: string;
+}
+
+export class RunWorkFlowRequestDto {
+  order_code: string;
   location: string;
 }
 
-export class InitiateWorkFlowInstanceResponseDto {
-  workFlowInstanceId: string;
+export class WorkFlowExecutionResponseDto {
+  id: string;
+  status: string;
+  workflow_id: string;
 }
 
-export class RetrieveWorkFlowResponseDto {
-  status: string
+export class CreateQRLocationRequestDto {
+  name: string;
+  qr_code: string;
+}
+
+export class UpdateQRLocationRequestDto {
+  name: string;
+  qr_code: string;
+}
+export class QRLocationResponseDto {
+  id: string;
+  name: string;
+  qr_code: string;
 }
