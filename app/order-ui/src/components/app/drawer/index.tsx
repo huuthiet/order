8899874080e7c3d2ec57620d079b632c7ab62b73
QuyetThangDev <quyetthang0501@gmail.com>
2024-11-27 14:07:55 +0700

    fix add menu item button

--- app/order-ui/src/components/app/drawer/index.tsx
+++ app/order-ui/src/components/app/drawer/index.tsx
@@ -1,3 +1,2 @@
-export { default as AddMenuItemDrawer } from './add-menu-item-drawer'
 export { default as CartDrawer } from './cart-drawer'
 export { default as CheckoutCartDrawer } from './checkout-cart-drawer'
