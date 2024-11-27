d37a5c122a32832eb640bc23142742d87ed65e46
QuyetThangDev <quyetthang0501@gmail.com>
2024-11-27 14:05:19 +0700

    Adjust logo

--- app/order-ui/src/components/app/drawer/index.tsx
+++ app/order-ui/src/components/app/drawer/index.tsx
@@ -1,2 +1,3 @@
+export { default as AddMenuItemDrawer } from './add-menu-item-drawer'
 export { default as CartDrawer } from './cart-drawer'
 export { default as CheckoutCartDrawer } from './checkout-cart-drawer'
