3661a8c315ae2d02678168d475e8e7bc3187347e
doduongthaituan <doduongthaituan@201102@gmail.com>
2024-12-02 14:33:35 +0700

    fix missing GoogleDriveService

--- app/order-api/src/db/db.controller.spec.ts
+++ app/order-api/src/db/db.controller.spec.ts
@@ -3,6 +3,7 @@ import { DbController } from './db.controller';
 import { DbService } from './db.service';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ConfigService } from '@nestjs/config';
+import { GoogleDriveService } from './google-drive.service';
 
 describe('DbController', () => {
   let controller: DbController;
@@ -12,6 +13,7 @@ describe('DbController', () => {
       controllers: [DbController],
       providers: [
         DbService,
+        GoogleDriveService,
         {
           provide: WINSTON_MODULE_NEST_PROVIDER,
           useValue: console, // Mock logger (or a custom mock)
--- app/order-api/src/db/db.service.spec.ts
+++ app/order-api/src/db/db.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbService } from './db.service';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
+import { GoogleDriveService } from './google-drive.service';
 
 describe('DbService', () => {
   let service: DbService;
@@ -9,10 +11,12 @@ describe('DbService', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DbService,
+        GoogleDriveService,
         {
           provide: WINSTON_MODULE_NEST_PROVIDER,
           useValue: console, // Mock logger (or a custom mock)
         },
+
         {
           provide: ConfigService,
           useValue: {
