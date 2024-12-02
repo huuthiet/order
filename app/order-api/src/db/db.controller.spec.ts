655011e0a45a2e4a8b8c20d6ed141ddf86e68c06
doduongthaituan <doduongthaituan@201102@gmail.com>
2024-12-02 14:26:43 +0700

    fix unit error

--- app/order-api/src/db/db.controller.spec.ts
+++ app/order-api/src/db/db.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbController } from './db.controller';
 import { DbService } from './db.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 describe('DbController', () => {
   let controller: DbController;
@@ -8,7 +9,13 @@ describe('DbController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DbController],
-      providers: [DbService],
+      providers: [
+        DbService,
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: console, // Mock logger (or a custom mock)
+        },
+      ],
     }).compile();
 
     controller = module.get<DbController>(DbController);
--- app/order-api/src/db/db.service.spec.ts
+++ app/order-api/src/db/db.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbService } from './db.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 describe('DbService', () => {
   let service: DbService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DbService],
+      providers: [
+        DbService,
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: console, // Mock logger (or a custom mock)
+        },
+      ],
     }).compile();
 
     service = module.get<DbService>(DbService);
