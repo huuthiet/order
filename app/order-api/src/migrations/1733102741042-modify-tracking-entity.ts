284bbd9319259340b541eaa6709be5d408816c68
huu thiet <huuthietn01@gmail.com>
2024-12-02 08:36:22 +0700

    add new migration

--- app/order-api/src/migrations/1733102741042-modify-tracking-entity.ts
+++ app/order-api/src/migrations/1733102741042-modify-tracking-entity.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class ModifyTrackingEntity1732847368723 implements MigrationInterface {
-    name = 'ModifyTrackingEntity1732847368723'
+export class ModifyTrackingEntity1733102741042 implements MigrationInterface {
+    name = 'ModifyTrackingEntity1733102741042'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`IDX_c18ab30e5fe911bae91a6c02e4\` ON \`order_tbl\``);
+        await queryRunner.query(`DROP INDEX \`IDX_0fec0873c97f173319b38957bf\` ON \`order_tbl\``);
         await queryRunner.query(`ALTER TABLE \`tracking_tbl\` CHANGE \`work_flow_instance_column\` \`workflow_execution_column\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`tracking_tbl\` DROP COLUMN \`workflow_execution_column\``);
         await queryRunner.query(`ALTER TABLE \`tracking_tbl\` ADD \`workflow_execution_column\` varchar(255) NULL`);
@@ -16,7 +16,7 @@ export class ModifyTrackingEntity1732847368723 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`tracking_tbl\` DROP COLUMN \`workflow_execution_column\``);
         await queryRunner.query(`ALTER TABLE \`tracking_tbl\` ADD \`workflow_execution_column\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`tracking_tbl\` CHANGE \`workflow_execution_column\` \`work_flow_instance_column\` varchar(255) NULL`);
-        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_c18ab30e5fe911bae91a6c02e4\` ON \`order_tbl\` (\`payment_column\`)`);
+        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_0fec0873c97f173319b38957bf\` ON \`order_tbl\` (\`invoice_column\`)`);
     }
 
 }
