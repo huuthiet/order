8087c96350e9cad1342c7c861a04aae460dc5f22
doduongthaituan <doduongthaituan@201102@gmail.com>
2024-12-09 15:03:26 +0700

    fix unit test running error

--- app/order-api/src/pdf/pdf.service.spec.ts
+++ app/order-api/src/pdf/pdf.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PdfService } from './pdf.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 describe('PdfService', () => {
   let service: PdfService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PdfService],
+      providers: [
+        PdfService,
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: console,
+        },
+      ],
     }).compile();
 
     service = module.get<PdfService>(PdfService);
