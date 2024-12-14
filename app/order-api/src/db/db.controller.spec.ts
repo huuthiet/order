af6ab961ab3c9a12bbe5b223ae8c3a53bd6e4ae0
doduongthaituan <doduongthaituan@201102@gmail.com>
2024-12-14 11:29:19 +0700

    fix error when running db test suites

--- app/order-api/src/db/db.controller.spec.ts
+++ app/order-api/src/db/db.controller.spec.ts
@@ -4,6 +4,12 @@ import { DbService } from './db.service';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ConfigService } from '@nestjs/config';
 import { GoogleDriveService } from './google-drive.service';
+import { SystemConfigService } from 'src/system-config/system-config.service';
+import { SystemConfig } from 'src/system-config/system-config.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { repositoryMockFactory } from 'src/test-utils/repository-mock.factory';
+import { mapperMockFactory } from 'src/test-utils/mapper-mock.factory';
+import { MAPPER_MODULE_PROVIDER } from 'src/app/app.constants';
 
 describe('DbController', () => {
   let controller: DbController;
@@ -14,9 +20,22 @@ describe('DbController', () => {
       providers: [
         DbService,
         GoogleDriveService,
+        SystemConfigService,
+        {
+          provide: getRepositoryToken(SystemConfig),
+          useFactory: repositoryMockFactory,
+        },
         {
           provide: WINSTON_MODULE_NEST_PROVIDER,
           useValue: console, // Mock logger (or a custom mock)
+        },
+        {
+          provide: MAPPER_MODULE_PROVIDER,
+          useValue: mapperMockFactory,
+        },
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: console,
         },
         {
           provide: ConfigService,
