184575a011b9cc99cb35f7db615714ea416b2e6e
huu thiet <huuthietn01@gmail.com>
2024-12-18 14:17:48 +0700

    add: api get all trackings; use new create tracking

--- app/order-api/src/tracking/tracking.dto.ts
+++ app/order-api/src/tracking/tracking.dto.ts
@@ -1,7 +1,7 @@
 import { AutoMap } from "@automapper/classes";
 import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { ArrayNotEmpty, IsArray, IsEnum, IsNotEmpty, ValidateNested } from "class-validator";
+import { Transform, Type } from "class-transformer";
+import { ArrayNotEmpty, IsArray, IsEnum, IsNotEmpty, IsOptional, Min, ValidateNested } from "class-validator";
 import { BaseResponseDto } from "src/app/base.dto";
 import { CreateTrackingOrderItemRequestDto, TrackingOrderItemResponseDto } from "src/tracking-order-item/tracking-order-item.dto";
 import { TrackingType, WorkflowStatus } from "./tracking.constants";
@@ -43,4 +43,52 @@ export class ChangeStatusRequestDto {
   @IsNotEmpty({ message: 'Invalid status of tracking' })
   @IsEnum(WorkflowStatus, { message: 'Invalid status of tracking' })
   status: string;
+}
+
+export class GetTrackingRequestDto {
+  @AutoMap()
+  @ApiProperty({
+    description: 'Tracking status',
+    required: false,
+  })
+  @IsOptional()
+  @Transform(({ value }) =>
+    typeof value === 'string' ? value.split(',') : [value],
+  )
+  status: string[] = [];
+
+  @AutoMap()
+  @ApiProperty({
+    description: 'Enable paging',
+    required: false,
+    type: Boolean,
+  })
+  @IsOptional()
+  @Transform(({ value }) => {
+    if (value === undefined || value === null) return true; // Default true
+    return value === 'true'; // Transform 'true' to `true` and others to `false`
+  })
+  hasPaging?: boolean;
+
+@AutoMap()
+  @ApiProperty({
+    example: 1,
+    description: 'Page number',
+    required: false,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1)
+  page: number = 1;
+
+  @AutoMap()
+  @ApiProperty({
+    example: 10,
+    description: 'Number of items per page',
+    required: false,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1)
+  size: number = 10;
 }
