b0503d65a76d1f3619a6d488dc7f11f714e2adcb
doduongthaituan <doduongthaituan@201102@gmail.com>
2024-12-26 16:45:34 +0700

    fix unit tests error

--- app/order-api/src/revenue/revenue.controller.spec.ts
+++ app/order-api/src/revenue/revenue.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RevenueController } from './revenue.controller';
 import { RevenueService } from './revenue.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { repositoryMockFactory } from 'src/test-utils/repository-mock.factory';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Revenue } from './revenue.entity';
+import { MAPPER_MODULE_PROVIDER } from 'src/app/app.constants';
 
 describe('RevenueController', () => {
   let controller: RevenueController;
@@ -8,7 +13,21 @@ describe('RevenueController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RevenueController],
-      providers: [RevenueService],
+      providers: [
+        RevenueService,
+        {
+          provide: MAPPER_MODULE_PROVIDER,
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Revenue),
+          useValue: repositoryMockFactory,
+        },
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: console,
+        },
+      ],
     }).compile();
 
     controller = module.get<RevenueController>(RevenueController);
