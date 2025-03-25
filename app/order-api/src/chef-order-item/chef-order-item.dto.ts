2dd4920224adf05ef25a11aa9d68bb82144aa0c1
huu thiet <huuthietn01@gmail.com>
2025-03-25 10:38:19 +0700

    add: api update multi chef order items; api get all chef order

--- app/order-api/src/chef-order-item/chef-order-item.dto.ts
+++ app/order-api/src/chef-order-item/chef-order-item.dto.ts
@@ -1,10 +1,17 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNotEmpty } from 'class-validator';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
 import { BaseResponseDto } from 'src/app/base.dto';
 import { ChefOrderResponseDto } from 'src/chef-order/chef-order.dto';
 import { OrderItemResponseDto } from 'src/order-item/order-item.dto';
 import { ChefOrderItemStatus } from './chef-order-item.constants';
+import { Type } from 'class-transformer';
 
 export class ChefOrderItemResponseDto extends BaseResponseDto {
   @AutoMap()
@@ -32,4 +39,34 @@ export class UpdateChefOrderItemRequestDto {
     message: 'Status must be in-progress or completed',
   })
   status: string;
+}
+
+export class UpdateMultiChefOrderItemRequestDto {
+  @AutoMap()
+  @ApiProperty({
+    description: 'The slug of branch',
+    example: 'in-progress',
+    required: false,
+  })
+  @IsNotEmpty({ message: 'Status is required' })
+  @IsEnum(ChefOrderItemStatus, {
+    message: 'Status must be in-progress or completed',
+  })
+  status: string;
+
+  @AutoMap()
+  @ApiProperty({
+    description: 'The array of chef order item slugs',
+    required: true,
+    example: ['chef-order-item-slug'],
+  })
+  @IsArray({
+    message: 'The slug array of chef order item slugs must be an array',
+  })
+  @ArrayNotEmpty({
+    message: 'The slug array of chef order item slugs must not be empty',
+  })
+  @IsString({ each: true, message: 'Each slug in the array must be a string' })
+  @Type(() => String)
+  chefOrderItems: string[];
 }
