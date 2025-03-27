b5822f10e74f61b417092e11b533e9bf2ebb095d
ladat <ladat01626362980@gmail.com>
2025-03-27 10:59:21 +0700

    allow select brand for chef area

--- app/order-ui/src/app/layouts/client/components/client-footer.tsx
+++ app/order-ui/src/app/layouts/client/components/client-footer.tsx
@@ -4,39 +4,39 @@ import { useTranslation } from 'react-i18next';
 import GoogleMap from './google-map';
 import { Logo } from '@/assets/images'
 import { ROUTE } from '@/constants'
+import { Mail, Phone } from 'lucide-react';
 
 export function ClientFooter() {
   const { t } = useTranslation('sidebar')
   const navigator = useNavigate()
+  const mail: string = "trend.coffee.tea@gmail.com"
   return (
     <footer className="text-white bg-primary">
       <div className="container pt-6 pb-6 md:pb-2">
         <div className="grid items-start grid-cols-2 gap-5 lg:grid-cols-4">
           <div className="flex flex-col items-start justify-center gap-2 w-fit">
             <span className="font-bold">
-              {t('footer.quickLink')}
+              {t('footer.introduction')}
             </span>
             <span className="text-sm cursor-pointer hover:underline" onClick={() => navigator(ROUTE.HOME)}> {t('footer.home')}</span>
             <span className="text-sm cursor-pointer hover:underline" onClick={() => navigator(ROUTE.CLIENT_MENU)}> {t('footer.menu')}</span>
-          </div>
-          <div className="flex flex-col items-start justify-center gap-2 w-fit">
-            <span className="font-bold">
-              {t('footer.introduction')}
-            </span>
             <span className="text-sm cursor-pointer hover:underline" onClick={() => navigator(ROUTE.ABOUT)}> {t('footer.aboutMe')}</span>
-            {/* <span className="text-sm">{t('footer.menu')}</span>
-            <span className="text-sm">{t('footer.product')}</span>
-            <span className="text-sm">{t('footer.offer')}</span>
-            <span className="text-sm">{t('footer.store')}</span>
-            <span className="text-sm">{t('footer.recruitment')}</span> */}
           </div>
           <div className="flex flex-col items-start justify-center gap-2 w-fit">
-            <span className="font-bold">
-              {t('footer.policy')}
-            </span>
-            <span className="text-sm cursor-pointer" onClick={() => navigator(ROUTE.POLICY)}>
-              {t('footer.securityTerm')}
-            </span>
+            <span className="font-bold">{t('footer.policy')}</span>
+            <span className="text-sm cursor-pointer" onClick={() => navigator(ROUTE.POLICY)}>{t('footer.securityTerm')}</span>
+         
+          </div>
+          <div className="flex flex-col items-start justify-center gap-4 w-fit">
+            <div className='flex gap-4'> <Phone /> <span className='cursor-pointer'>{t('footer.contact')}: <b>0888022200</b></span></div>
+            <div className='flex gap-4'>
+              <Mail />
+              <span className='cursor-pointer hover:underline' onClick={() => window.location.href = `mailto:${mail}`}>
+                <b>{mail}</b>
+              </span>
+            </div>
+
+
             {/* <span className="text-sm">Chính sách bảo mật</span> */}
           </div>
           <div className="relative flex flex-col items-start justify-center col-span-1 gap-4">
