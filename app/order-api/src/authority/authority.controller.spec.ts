d187a9720d857f6e65ee4217b5a1a30908144549
tuanhcmute <20110743@student.hcmute.edu.vn>
2025-03-31 14:17:56 +0700

    update authority, authority group api

--- app/order-api/src/authority/authority.controller.spec.ts
+++ app/order-api/src/authority/authority.controller.spec.ts
@@ -1,6 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorityController } from './authority.controller';
 import { AuthorityService } from './authority.service';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { MAPPER_MODULE_PROVIDER } from 'src/app/app.constants';
+import { mapperMockFactory } from 'src/test-utils/mapper-mock.factory';
+import { repositoryMockFactory } from 'src/test-utils/repository-mock.factory';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Authority } from './authority.entity';
+import { dataSourceMockFactory } from 'src/test-utils/datasource-mock.factory';
+import { TransactionManagerService } from 'src/db/transaction-manager.service';
+import { DataSource } from 'typeorm';
 
 describe('AuthorityController', () => {
   let controller: AuthorityController;
@@ -8,7 +17,26 @@ describe('AuthorityController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthorityController],
-      providers: [AuthorityService],
+      providers: [
+        AuthorityService,
+        TransactionManagerService,
+        {
+          provide: DataSource,
+          useFactory: dataSourceMockFactory,
+        },
+        {
+          provide: getRepositoryToken(Authority),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: WINSTON_MODULE_NEST_PROVIDER,
+          useValue: console,
+        },
+        {
+          provide: MAPPER_MODULE_PROVIDER,
+          useFactory: mapperMockFactory,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthorityController>(AuthorityController);
--- app/order-api/src/authority/authority.validation.ts
+++ app/order-api/src/authority/authority.validation.ts
@@ -1,11 +1,31 @@
 import { TErrorCodeValue } from 'src/app/app.validation';
 
-export type TAuthorityErrorCodeKey = null;
+export const AUTHORITY_NOT_FOUND = 'AUTHORITY_NOT_FOUND';
+export const AUTHORITY_UPDATE_FAILED = 'AUTHORITY_UPDATE_FAILED';
+export const AUTHORITY_DELETE_FAILED = 'AUTHORITY_DELETE_FAILED';
 
-// 153001 - 155000
+export type TAuthorityErrorCodeKey =
+  | typeof AUTHORITY_NOT_FOUND
+  | typeof AUTHORITY_UPDATE_FAILED
+  | typeof AUTHORITY_DELETE_FAILED;
+
+// 156001- 156500
 export type TAuthorityErrorCode = Record<
   TAuthorityErrorCodeKey,
   TErrorCodeValue
 >;
 
-export const AuthorityValidation: TAuthorityErrorCode = {};
+export const AuthorityValidation: TAuthorityErrorCode = {
+  [AUTHORITY_NOT_FOUND]: {
+    code: 156001,
+    message: 'Authority not found',
+  },
+  [AUTHORITY_UPDATE_FAILED]: {
+    code: 156002,
+    message: 'Authority update failed',
+  },
+  [AUTHORITY_DELETE_FAILED]: {
+    code: 156003,
+    message: 'Authority delete failed',
+  },
+};
