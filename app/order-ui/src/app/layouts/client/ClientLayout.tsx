bf80489d0be519b85eb2b82fd7077223a8e27ed0
ladat <ladat01626362980@gmail.com>
2025-04-04 15:10:46 +0700

    fix history url

--- app/order-ui/src/app/layouts/client/ClientLayout.tsx
+++ app/order-ui/src/app/layouts/client/ClientLayout.tsx
@@ -1,25 +1,30 @@
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 import { useIsMobile } from '@/hooks/use-mobile'
 import { cn } from '@/lib'
 import { DownloadProgress } from '@/components/app/progress'
-import { useDownloadStore, usePaymentMethodStore } from '@/stores'
+import { useDownloadStore, usePaymentMethodStore, useUserStore } from '@/stores'
 import { ClientHeader, ClientFooter, BackToTop, BottomBar } from './components'
 import { ChooseBranchDialog } from '@/components/app/dialog'
 import { useEffect } from 'react'
-import { ROUTE } from '@/constants'
+import { Role, ROUTE } from '@/constants'
 
 export default function ClientLayout() {
   const isMobile = useIsMobile()
   const { progress, fileName, isDownloading } = useDownloadStore()
   const location = useLocation()
   const { clearStore } = usePaymentMethodStore()
+  const { userInfo } = useUserStore();
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (!location.pathname.startsWith(ROUTE.CLIENT_PAYMENT)) {
       clearStore()
     }
-  }, [location.pathname, clearStore])
+    if (userInfo && userInfo.role.name !== Role.CUSTOMER) {
+      navigate(ROUTE.LOGIN)
+    }
+  }, [location.pathname, clearStore, userInfo, navigate])
 
   return (
     <div className="flex flex-col min-h-screen">
