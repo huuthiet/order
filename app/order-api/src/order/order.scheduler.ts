ca434047417f102ccc8fa3a16e5bae908fb31db0
huu thiet <huuthietn01@gmail.com>
2025-04-11 10:49:20 +0700

    refix time run order scheduler

--- app/order-api/src/order/order.scheduler.ts
+++ app/order-api/src/order/order.scheduler.ts
@@ -52,6 +52,10 @@ export class OrderScheduler {
   async updateReferenceNumberForPaidOrdersAndInvoices() {
     const context = `${OrderScheduler.name}.${this.updateReferenceNumberForPaidOrdersAndInvoices.name}`;
 
+    this.logger.log(
+      `Start update reference number for paid orders and invoices`,
+      context,
+    );
     const branches = await this.branchRepository.find();
     const updatedOrders: Order[] = [];
     for (const branch of branches) {
