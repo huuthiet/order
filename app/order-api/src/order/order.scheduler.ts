c93f521714f542a103f48b34a8f49c0492479bd5
huu thiet <huuthietn01@gmail.com>
2025-04-17 10:02:21 +0700

    uncomment for save order when update reference number in scheduler order

--- app/order-api/src/order/order.scheduler.ts
+++ app/order-api/src/order/order.scheduler.ts
@@ -92,7 +92,7 @@ export class OrderScheduler {
         }
         updatedOrders.push(...orders);
       }
-      // await this.orderRepository.save(updatedOrders);
+      await this.orderRepository.save(updatedOrders);
       this.logger.log(
         `Update reference number for orders: ${updatedOrders.length}`,
         context,
