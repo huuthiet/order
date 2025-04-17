e3d00abf1dc77bb89e3e3d70fd3c85f66d14d009
QuyetThangDev <quyetthang0501@gmail.com>
2025-04-17 16:57:25 +0700

    Fix overview detail page layout error

--- app/order-ui/src/app/client/about/page.tsx
+++ app/order-ui/src/app/client/about/page.tsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
 
   if (isLoading) {
     return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <p className="text-lg font-medium text-gray-500">Loading...</p>
       </div>
     )
@@ -16,7 +16,7 @@ export default function AboutPage() {
 
   if (!staticPage || !staticPage.result) {
     return (
-      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <p className="text-lg font-medium text-gray-500">
           {t('about.noContent')}
         </p>
@@ -26,16 +26,16 @@ export default function AboutPage() {
 
   return (
     <div className="min-h-screen bg-white">
-      <header className="bg-gray-800 py-10">
+      <header className="py-10 bg-gray-800">
         <div className="container mx-auto text-center">
           <h1 className="text-3xl font-bold text-white">
             {staticPage.result.title}
           </h1>
         </div>
       </header>
-      <main className="ql-snow container mx-auto px-4 py-8">
+      <main className="container px-4 py-8 mx-auto ql-snow">
         <article
-          className="prose prose-lg ql-editor max-w-none text-xs"
+          className="max-w-none text-xs prose prose-lg ql-editor"
           dangerouslySetInnerHTML={{ __html: staticPage.result.content }}
         />
       </main>
