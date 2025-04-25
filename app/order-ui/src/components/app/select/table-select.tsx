5232451adaefe537c8ef43c6d1ce0f6d6bae9fcc
QuyetThangDev <quyetthang0501@gmail.com>
2025-04-25 18:06:11 +0700

    Fix sorting in table select

--- app/order-ui/src/components/app/select/table-select.tsx
+++ app/order-ui/src/components/app/select/table-select.tsx
@@ -40,12 +40,13 @@ export default function TableSelect({ tableOrder, onTableSelect }: ITableSelectP
             setSelectedTableId(tableOrder.slug)
         }
     }, [tableOrder])
-    const tableList = tables?.result.sort((a, b) => {
+    const tableList = [...(tables?.result || [])].sort((a, b) => {
         if (a.status !== b.status) {
-            return a.status.localeCompare(b.status); // Đảo ngược status (RESERVED trước AVAILABLE)
+            // Đảo ngược thứ tự: RESERVED < AVAILABLE
+            return a.status === TableStatus.RESERVED ? -1 : 1
         }
-        return Number(a.name) - Number(b.name); // Sắp xếp theo tên nếu status giống nhau
-    }) || [];
+        return Number(a.name) - Number(b.name)
+    })
 
     if (cartItems?.type === OrderTypeEnum.TAKE_OUT) {
         return null
