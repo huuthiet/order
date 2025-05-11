8ae7e83ec002bfa547f6907fb8c67da491eac764
huu thiet <huuthietn01@gmail.com>
2025-05-12 06:59:26 +0700

    add: voucher group scheduler

--- app/order-api/src/voucher-group/voucher-group.scheduler.ts
+++ app/order-api/src/voucher-group/voucher-group.scheduler.ts
@@ -1,8 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
-// import { VoucherGroup } from './voucher-group.entity';
-// import { IsNull } from 'typeorm';
+import { VoucherGroup } from './voucher-group.entity';
+import { IsNull } from 'typeorm';
 import { Logger } from '@nestjs/common';
-// import { Timeout } from '@nestjs/schedule';
+import { Timeout } from '@nestjs/schedule';
 import { VoucherUtils } from 'src/voucher/voucher.utils';
 import { TransactionManagerService } from 'src/db/transaction-manager.service';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
@@ -16,40 +16,40 @@ export class VoucherGroupScheduler {
     private readonly logger: Logger,
   ) {}
 
-  // @Timeout(1000)
-  // async createVoucherGroupForVouchersHaveNoGroup() {
-  //   const context = `${VoucherGroupScheduler.name}.${this.createVoucherGroupForVouchersHaveNoGroup.name}`;
-  //   const vouchers = await this.voucherUtils.getBulkVouchers({
-  //     where: {
-  //       voucherGroup: IsNull(),
-  //     },
-  //   });
+  @Timeout(1000)
+  async createVoucherGroupForVouchersHaveNoGroup() {
+    const context = `${VoucherGroupScheduler.name}.${this.createVoucherGroupForVouchersHaveNoGroup.name}`;
+    const vouchers = await this.voucherUtils.getBulkVouchers({
+      where: {
+        voucherGroup: IsNull(),
+      },
+    });
 
-  //   const voucherGroups = [];
+    const voucherGroups = [];
 
-  //   for (const voucher of vouchers) {
-  //     const voucherGroup = new VoucherGroup();
-  //     voucherGroup.title = voucher.title;
-  //     voucherGroup.description = voucher.description;
-  //     voucherGroup.vouchers = [voucher];
-  //     voucherGroups.push(voucherGroup);
-  //   }
+    for (const voucher of vouchers) {
+      const voucherGroup = new VoucherGroup();
+      voucherGroup.title = voucher.title;
+      voucherGroup.description = voucher.description;
+      voucherGroup.vouchers = [voucher];
+      voucherGroups.push(voucherGroup);
+    }
 
-  //   await this.transactionService.execute(
-  //     async (manager) => await manager.save(voucherGroups),
-  //     (result) => {
-  //       this.logger.log(
-  //         `Created ${result.length} voucher groups for ${vouchers.length} vouchers`,
-  //         context,
-  //       );
-  //     },
-  //     (error) => {
-  //       this.logger.error(
-  //         `Failed to create voucher group for ${vouchers.length} vouchers`,
-  //         error.stack,
-  //         context,
-  //       );
-  //     },
-  //   );
-  // }
+    await this.transactionService.execute(
+      async (manager) => await manager.save(voucherGroups),
+      (result) => {
+        this.logger.log(
+          `Created ${result.length} voucher groups for ${vouchers.length} vouchers`,
+          context,
+        );
+      },
+      (error) => {
+        this.logger.error(
+          `Failed to create voucher group for ${vouchers.length} vouchers`,
+          error.stack,
+          context,
+        );
+      },
+    );
+  }
 }
